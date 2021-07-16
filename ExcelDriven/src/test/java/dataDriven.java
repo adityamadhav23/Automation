abe4cb51fa520b33ec65674e1ecb2aa426ce5a20
adityamadhav23 <adityamadhav23@gmail.com>
2021-07-16 18:45:00 +0530

    Excel Driven

--- ExcelDriven/src/test/java/dataDriven.java
+++ ExcelDriven/src/test/java/dataDriven.java
@@ -1,6 +1,7 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -9,9 +10,16 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class dataDriven {
-
+	
+	
 	public static void main(String[] args) throws IOException {
+				}
+
+
+	public ArrayList<String> getData(String testCaseName) throws IOException {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		ArrayList<String> a=new ArrayList<String>();
 		FileInputStream fis= new FileInputStream("D://programmes//eclipse-workspace//ExcelDriven//demodata.xlsx");
 		
 		XSSFWorkbook workbook=new XSSFWorkbook(fis);
@@ -24,6 +32,7 @@ public class dataDriven {
 			Iterator<Row> rows=sheet.iterator();
 			Row firstrow=rows.next();
 			Iterator<Cell> ce=firstrow.cellIterator();
+			//To get all the column names and search TestCases
 			int k=0;
 			int column=0;
 			while(ce.hasNext()){
@@ -33,9 +42,23 @@ public class dataDriven {
 				k++;
 				}
 			System.out.println(column);
+			
+		//once column is identified then scan entire testcase column to identify purchase testcase row
+			while(rows.hasNext()) {
+				Row r=rows.next();
+				if(r.getCell(column).getStringCellValue().equalsIgnoreCase("Purchase")) {
+					Iterator<Cell> cv=r.cellIterator();
+					while(cv.hasNext()) {
+						a.add(cv.next().getStringCellValue());
+					}
+				}
 			}
 			}
-		}
+			}
+		return a;
+
+
+	}
 	}
 
 
