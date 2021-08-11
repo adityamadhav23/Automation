4a53d4c2f5402ad8ce28a56d637d7acd54cc5225
adityamadhav23 <adityamadhav23@gmail.com>
2021-08-12 00:05:05 +0530

    Final Automation project

---
+++ E2EProject/src/main/java/resources/base.java
@@ -0,0 +1,35 @@
+package resources;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class base {
+	public WebDriver driver;
+	public WebDriver initalizeDriver() throws IOException {
+		Properties properties=new Properties();
+		FileInputStream fis=new FileInputStream("D:\\programmes\\eclipse-workspace\\E2EProject\\src\\main\\java\\resources\\data.properties");
+		properties.load(fis);
+		String browser=properties.getProperty("browser");
+		
+		if (browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "D:\\programmes\\selenium\\chromedriver_win32\\chromedriver.exe");
+			
+			driver=new ChromeDriver();
+		}else if (browser.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", "D:\\programmes\\selenium\\geckodriver-v0.29.1-win32\\geckodriver.exe");
+			driver=new FirefoxDriver();
+		}else if (browser.equals("edge")) {
+			System.setProperty("webdriver.edge.driver", "D:\\\\programmes\\\\selenium\\\\edgedriver_win64\\MicrosoftWebDriver.exe");
+			driver=new EdgeDriver();
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}
+}
--- E2EProject/src/test/java/Automation/HomePage.java
+++ E2EProject/src/test/java/Automation/HomePage.java
@@ -5,14 +5,24 @@ import java.io.IOException;
 
 import org.testng.annotations.Test;
 
-import Automation.base;
+import pageObjects.LandingPage;
+import pageObjects.LoginPage;
+import resources.base;
 
 public class HomePage extends base{
 	
 @Test
 public void basePageNavigation() throws IOException{
 	driver=initalizeDriver();
-	driver.get("http://www.google.com");
+	driver.get("http://www.qaclickacademy.com/");
+	
+	LandingPage l=new LandingPage(driver);
+	l.getLogin().click();
+	
+	LoginPage lPage=new LoginPage(driver);
+	lPage.getEmail().sendKeys("adi");
+	lPage.getPassword().sendKeys("adi");
+	lPage.getlogin().click();
 }
 
 }
