84a16420e618321ea9c7d0b6f47667516013a0a9
adityamadhav23 <adityamadhav23@gmail.com>
2021-08-12 17:53:25 +0530

    Adding multiple data from array

--- E2EProject/src/test/java/Automation/HomePage.java
+++ E2EProject/src/test/java/Automation/HomePage.java
@@ -3,6 +3,7 @@ package Automation;
 
 import java.io.IOException;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
@@ -11,8 +12,8 @@ import resources.base;
 
 public class HomePage extends base{
 	
-@Test
-public void basePageNavigation() throws IOException{
+@Test(dataProvider = "getData")
+public void basePageNavigation(String Username, String Passsowrd, String text) throws IOException{
 	driver=initalizeDriver();
 	driver.get("http://www.qaclickacademy.com/");
 	
@@ -20,9 +21,28 @@ public void basePageNavigation() throws IOException{
 	l.getLogin().click();
 	
 	LoginPage lPage=new LoginPage(driver);
-	lPage.getEmail().sendKeys("adi");
-	lPage.getPassword().sendKeys("adi");
+	lPage.getEmail().sendKeys(Username);
+	lPage.getPassword().sendKeys(Passsowrd);
 	lPage.getlogin().click();
+	System.out.println(text);
+}
+@DataProvider
+public Object[][] getData() {
+	//row>>different data types test should run
+	//column>>how many values per test
+	Object[][] data=new Object[2][2];
+	
+	//1st data set
+	data[0][0]="adi@test.com"; //getEmail
+	data[0][1]="adi";	//getPassword
+	//data[0][2]="Restricted User";
+	
+	//2nd data set
+	data[1][0]="aditya@test.com"; //getEmail
+	data[1][1]="aditya";	//getPassword
+	//data[1][2]="Restricted User";
+	
+	return data;
+}
 }
 
-}
